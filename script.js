67a670ff3c09b21e6eca1fbca2eb1ff1956fa929
Michele Cozzola <cozzolamichele89@gmail.com>
2022-03-09 12:08:28 +0100

    end

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 const findMyState = () =>{
     let light = true
-    
+
     let cityDom = document.querySelector('#city')
     const status =  document.querySelector('.status');
     const success = (position) => {
-        console.log(position);
+        /* console.log(position); */
         const latitude = /* 37.9908372 */ position.coords.latitude;
         const longitude = /* 23.7383394 */ position.coords.longitude;
         
@@ -15,10 +15,9 @@ const findMyState = () =>{
         .then(res => res.json())
         .then(data => {
             cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
-            console.log(data);
-            console.log(`${data.localityInfo.administrative[3].name}`);
+            /* console.log(data); */
+           /*  console.log(`${data.localityInfo.administrative[3].name}`); */
         })             
-        
         
         const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
         
@@ -29,18 +28,15 @@ const findMyState = () =>{
             let celsius = Math.floor(temp - 273.15)
             let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
             //cityDom.innerHTML = `${data.name}`
-            console.log(data);
+            /* console.log(data); */
             
             var iconCode = data.weather[0].icon;            
             document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
-            
         })
-        
-        
-        
         let morning = document.querySelector('.morning')
-        let night = document.querySelector('.fa-moon')
+        let night = document.querySelector('.night')
         let container = document.querySelector('.container')
+        let toggleMode = document.querySelector('#toggle_mode')
         
         const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
     ];
@@ -48,26 +44,27 @@ const findMyState = () =>{
     
     setInterval(()=>{
     var d = new Date()
-    let numData = d.getDate()
-    let month = monthNames[d.getMonth()]
-    let day = dayNames[d.getDay()]
-    let year = d.getFullYear()
-    const hours = d.getHours()
-    const minutes = d.getMinutes()
-    const seconds = d.getSeconds()
+    var numData = d.getDate()
+    var month = monthNames[d.getMonth()]
+    var day = dayNames[d.getDay()]
+    var year = d.getFullYear()
+    var hours = d.getHours()
+    var minutes = d.getMinutes()
+    var seconds = d.getSeconds()
     
     const hour = 30*hours + minutes/2
     const minute = 6 * minutes
     const second = 6 * seconds 
-    /* var gmtHours = -d.getTimezoneOffset()/60; */
+    /* var gmtHours = -d.getTimezoneOffset()/60; */  
     
     let calendar = document.querySelector('#calendar')
     calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
     
     /* interval  */
         navigator.getBattery().then(function (battery) {
-            let level = battery.level;        
-            document.querySelector('#battery').innerHTML = (level*100) + '%';
+            let level = battery.level;   
+            /* console.log;    */  
+            document.querySelector('#battery').innerHTML = (Math.floor(level *  100)) + '%';
             let batteryIcon = document.querySelector('.battery-icon');
             if(level <=0.10){
                 batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
@@ -96,6 +93,7 @@ const findMyState = () =>{
     },900);
 
     function toggle() {
+        
         morning.classList.toggle('toggle_active')
         night.classList.toggle('toggle_active')
         light = false
@@ -103,20 +101,23 @@ const findMyState = () =>{
     }
     
     morning.addEventListener('click', ()=>{
-        toggle()        
+        toggle()
+        toggleMode.innerHTML = 'DARK ON'    
     })
     
     night.addEventListener('click', ()=>{
-        toggle()        
+        toggle()
+        toggleMode.innerHTML = 'DARK OFF' 
+                 
     })
-    
-    
     
 }
 const error = () =>{
     status.textContent = `Unable to retrieve your location`
 }
+
 navigator.geolocation.getCurrentPosition(success,error);
 }
+
 document.addEventListener('DOMContentLoaded', findMyState);
 
