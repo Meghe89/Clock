8b87440429b43b2b0f45ff0843f01a6a599d575a
Michele Cozzola <cozzolamichele89@gmail.com>
2022-03-11 12:17:09 +0100

    refactoring

--- script.js
+++ script.js
@@ -1,48 +1,33 @@
+let light = true
+let morning = document.querySelector('.morning')
+let night = document.querySelector('.night')
+let container = document.querySelector('.container')
+let toggleMode = document.querySelector('#toggle_mode')
 
-const findMyState = () =>{
-    let light = true
-
-    let cityDom = document.querySelector('#city')
-    const status =  document.querySelector('.status');
-    const success = (position) => {
-        /* console.log(position); */
-        const latitude = /* 37.9908372 */ position.coords.latitude;
-        const longitude = /* 23.7383394 */ position.coords.longitude;
-        
-        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=it`
-        
-        fetch(geoApiUrl)
-        .then(res => res.json())
-        .then(data => {
-            cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
-            /* console.log(data); */
-           /*  console.log(`${data.localityInfo.administrative[3].name}`); */
-        })             
-        
-        const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
-        
-        fetch(meteoApi)
-        .then(res => res.json())
-        .then(data =>{
-            let temp = data.main.temp;
-            let celsius = Math.floor(temp - 273.15)
-            let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
-            //cityDom.innerHTML = `${data.name}`
-            /* console.log(data); */
-            
-            var iconCode = data.weather[0].icon;            
-            document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
-        })
-        let morning = document.querySelector('.morning')
-        let night = document.querySelector('.night')
-        let container = document.querySelector('.container')
-        let toggleMode = document.querySelector('#toggle_mode')
-        
-        const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
-    ];
-    const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
+function toggle() {
     
-    setInterval(()=>{
+    morning.classList.toggle('toggle_active')
+    night.classList.toggle('toggle_active')
+    light = false
+    container.classList.toggle('bg-dark')
+}
+
+morning.addEventListener('click', ()=>{
+    toggle()
+    toggleMode.innerHTML = 'DARK ON'    
+})
+
+night.addEventListener('click', ()=>{
+    toggle()
+    toggleMode.innerHTML = 'DARK OFF' 
+    
+})
+
+
+
+function data() {
+    const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+    const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
     var d = new Date()
     var numData = d.getDate()
     var month = monthNames[d.getMonth()]
@@ -61,63 +46,86 @@ const findMyState = () =>{
     calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
     
     /* interval  */
-        navigator.getBattery().then(function (battery) {
-            let level = battery.level;   
-            /* console.log;    */  
-            document.querySelector('#battery').innerHTML = (Math.floor(level *  100)) + '%';
-            let batteryIcon = document.querySelector('.battery-icon');
-            if(level <=0.10){
-                batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
-            } else if(level <= 0.25){
-                batteryIcon.classList.add('fas', 'fa-battery-quarter')
-            } else if(level <=0.50){
-                batteryIcon.classList.add('fas', 'fa-battery-half')
-            }else if(level <=0.80){
-                batteryIcon.classList.add('fas', 'fa-battery-three-quarters')
-            } else{
-                batteryIcon.classList.add('fas', 'fa-battery-full')
-            }        
-        });
+    
+    
+    if(hours >= 12){
+        var period = 'pm'
+    }else{
+        var period = 'am'
+    }
+    document.querySelector('.med').innerHTML = period;
+    document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
+    document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
+    document.querySelector('.sec').style.transform = `rotate(${second}deg)`
+}
 
-        if(hours >= 12){
-            var period = 'pm'
-        }else{
-            var period = 'am'
+navigator.getBattery().then(function a (battery) {
+    let level = battery.level;   
+    console.log(level);   
+    document.querySelector('#battery').innerHTML = (Math.floor(level *  100)) + '%';
+    let batteryIcon = document.querySelector('.battery-icon');
+    if(level <=0.10){
+        batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
+    } else if(level <= 0.25){
+        batteryIcon.classList.add('fas', 'fa-battery-quarter')
+    } else if(level <=0.50){
+        batteryIcon.classList.add('fas', 'fa-battery-half')
+    }else if(level <=0.80){
+        batteryIcon.classList.add('fas', 'fa-battery-three-quarters')
+    } else{
+        batteryIcon.classList.add('fas', 'fa-battery-full')
+    }        
+    
+    
+    const findMyState = () =>{
+        let cityDom = document.querySelector('#city')
+        const status =  document.querySelector('.status');
+        
+        const success = (position) => {
+            /* console.log(position); */
+            const latitude = /* 37.9908372 */ position.coords.latitude;
+            const longitude = /* 23.7383394 */ position.coords.longitude;
+            
+            const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=it`
+            
+            fetch(geoApiUrl)
+            .then(res => res.json())
+            .then(data => {
+                cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
+                /* console.log(data); */
+                /*  console.log(`${data.localityInfo.administrative[3].name}`); */
+            })             
+            
+            const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
+            
+            fetch(meteoApi)
+            .then(res => res.json())
+            .then(data =>{
+                let temp = data.main.temp;
+                let celsius = Math.floor(temp - 273.15)
+                let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
+                //cityDom.innerHTML = `${data.name}`
+                /* console.log(data); */
+                
+                var iconCode = data.weather[0].icon;
+                let wicon = document.querySelector("#wicon")
+                wicon.src =`http://openweathermap.org/img/w/${iconCode}.png`;
+                
+            })
+               
         }
-        document.querySelector('.med').innerHTML = period;
-        document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
-        document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
-        document.querySelector('.sec').style.transform = `rotate(${second}deg)`;
+        const error = () =>{
+            alert(`Unable to retrieve your location`)
+        }
         
-        
-    },900);
-
-    function toggle() {
-        
-        morning.classList.toggle('toggle_active')
-        night.classList.toggle('toggle_active')
-        light = false
-        container.classList.toggle('bg-dark')
+        navigator.geolocation.getCurrentPosition(success,error);
+        setInterval(()=>{        
+            data()        
+            a(battery)       
+            
+        },900);
     }
     
-    morning.addEventListener('click', ()=>{
-        toggle()
-        toggleMode.innerHTML = 'DARK ON'    
-    })
+    document.addEventListener('DOMContentLoaded', findMyState);
     
-    night.addEventListener('click', ()=>{
-        toggle()
-        toggleMode.innerHTML = 'DARK OFF' 
-                 
-    })
-    
-}
-const error = () =>{
-    status.textContent = `Unable to retrieve your location`
-}
-
-navigator.geolocation.getCurrentPosition(success,error);
-}
-
-document.addEventListener('DOMContentLoaded', findMyState);
-
+});
