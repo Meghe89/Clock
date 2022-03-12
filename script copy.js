697973901c3b21fe655babb627a62e2f9bc575a6
Michele Cozzola <cozzolamichele89@gmail.com>
2022-03-12 18:06:51 +0100

    back

--- script copy.js
+++ script copy.js
@@ -1,11 +1,12 @@
+/* global variable for toggle */
 let light = true
+/* dom selector for toggle */
 let morning = document.querySelector('.morning')
 let night = document.querySelector('.night')
 let container = document.querySelector('.container')
 let toggleMode = document.querySelector('#toggle_mode')
 
-function toggle() {
-    
+function toggle() {    
     morning.classList.toggle('toggle_active')
     night.classList.toggle('toggle_active')
     light = false
@@ -13,60 +14,19 @@ function toggle() {
 }
 
 morning.addEventListener('click', ()=>{
-    toggle()
-    toggleMode.innerHTML = 'DARK ON'    
+    toggle()        
 })
 
 night.addEventListener('click', ()=>{
-    toggle()
-    toggleMode.innerHTML = 'DARK OFF' 
-    
+    toggle()        
 })
 
-
-
-function data() {
-    const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
-
-    const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"];
-
-    var d = new Date()
-    var numData = d.getDate()
-    var month = monthNames[d.getMonth()]
-    var day = dayNames[d.getDay()]
-    var year = d.getFullYear()
-    var hours = d.getHours()
-    var minutes = d.getMinutes()
-    var seconds = d.getSeconds()
-    
-    const hour = 30*hours + minutes/2
-    const minute = 6 * minutes
-    const second = 6 * seconds 
-    /* var gmtHours = -d.getTimezoneOffset()/60; */  
-    
-    let calendar = document.querySelector('#calendar')
-    calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
-    
-    /* interval  */
-    
-    
-    if(hours >= 12){
-        var period = 'pm'
-    }else{
-        var period = 'am'
-    }
-    document.querySelector('.med').innerHTML = period;
-    document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
-    document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
-    document.querySelector('.sec').style.transform = `rotate(${second}deg)`
-    console.log(`Qui ho i dati DATE per la prima volta`);
-}
-
-navigator.getBattery().then(function a (battery) {
-    let level = battery.level;   
-    console.log(level);   
-    document.querySelector('#battery').innerHTML = (Math.floor(level *  100)) + '%';
+navigator.getBattery().then(function batt (battery) {
+    let level = battery.level;        
     let batteryIcon = document.querySelector('.battery-icon');
+    
+    document.querySelector('#battery').innerHTML = (Math.ceil(level *  100)) + '%';
+    
     if(level <=0.10){
         batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
     } else if(level <= 0.25){
@@ -79,14 +39,12 @@ navigator.getBattery().then(function a (battery) {
         batteryIcon.classList.add('fas', 'fa-battery-full')
     }        
     
-    console.log(`Qui ho i dati battery per la prima volta`);
-
-    const findMyState = () =>{
+    
+    const findMyState = () =>{        
         let cityDom = document.querySelector('#city')
         const status =  document.querySelector('.status');
-        
         const success = (position) => {
-            /* console.log(position); */
+            console.log(position);
             const latitude = /* 37.9908372 */ position.coords.latitude;
             const longitude = /* 23.7383394 */ position.coords.longitude;
             
@@ -95,48 +53,72 @@ navigator.getBattery().then(function a (battery) {
             fetch(geoApiUrl)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
                 cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
-                /* console.log(data); */
-                /*  console.log(`${data.localityInfo.administrative[3].name}`); */
-                console.log(`qui ho la localizzazione`);
+                console.log(data);
+                console.log(`${data.localityInfo.administrative[3].name}`);
             })             
+            
             
             const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
             
             fetch(meteoApi)
             .then(res => res.json())
             .then(data =>{
-                console.log(data);
                 let temp = data.main.temp;
                 let celsius = Math.floor(temp - 273.15)
                 let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
                 //cityDom.innerHTML = `${data.name}`
-                /* console.log(data); */
+                console.log(data);
                 
-                var iconCode = data.weather[0].icon;
-                let wicon = document.querySelector("#wicon")
-                wicon.src =`http://openweathermap.org/img/w/${iconCode}.png`;
-                console.log(`qui ho il meteo`);
+                var iconCode = data.weather[0].icon;            
+                document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
                 
             })
-               
+
+            const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+            const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
+            
+            setInterval(()=>{
+                var d = new Date()
+                let numData = d.getDate()
+                let month = monthNames[d.getMonth()]
+                let day = dayNames[d.getDay()]
+                let year = d.getFullYear()
+                const hours = d.getHours()
+                const minutes = d.getMinutes()
+                const seconds = d.getSeconds()
+                
+                const hour = 30*hours + minutes/2
+                const minute = 6 * minutes
+                const second = 6 * seconds 
+                /* var gmtHours = -d.getTimezoneOffset()/60; */
+                
+                let calendar = document.querySelector('#calendar')
+                calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
+                
+                /* interval  */
+                
+                
+                if(hours >= 12){
+                    var period = 'pm'
+                }else{
+                    var period = 'am'
+                }
+                document.querySelector('.med').innerHTML = period;
+                document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
+                document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
+                document.querySelector('.sec').style.transform = `rotate(${second}deg)`;
+                
+                batt(battery)
+                console.log((battery.level));
+            },900);   
         }
+
         const error = () =>{
             alert(`Unable to retrieve your location`)
         }
-        
         navigator.geolocation.getCurrentPosition(success,error);
-
-        setInterval(()=>{        
-            data() 
-            console.log(`Qui ho i dati DATE nell'interval`);       
-            a(battery)       
-            console.log(`Qui ho i battery DATE nell'interval`);       
-            
-        },900);
     }
-    
     document.addEventListener('DOMContentLoaded', findMyState);
     
 });
--- script.js
+++ script.js
@@ -1,124 +1,121 @@
-/* global variable for toggle */
-let light = true
-/* dom selector for toggle */
-let morning = document.querySelector('.morning')
-let night = document.querySelector('.night')
-let container = document.querySelector('.container')
-let toggleMode = document.querySelector('#toggle_mode')
+const findMyState = () =>{
+    let light = true
 
-function toggle() {    
-    morning.classList.toggle('toggle_active')
-    night.classList.toggle('toggle_active')
-    light = false
-    container.classList.toggle('bg-dark')
+    let cityDom = document.querySelector('#city')
+    const status =  document.querySelector('.status');
+    const success = (position) => {
+        /* console.log(position); */
+        const latitude = /* 37.9908372 */ position.coords.latitude;
+        const longitude = /* 23.7383394 */ position.coords.longitude;
+        
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=it`
+        
+        fetch(geoApiUrl)
+        .then(res => res.json())
+        .then(data => {
+            cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
+            /* console.log(data); */
+           /*  console.log(`${data.localityInfo.administrative[3].name}`); */
+        })             
+        
+        const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
+        
+        fetch(meteoApi)
+        .then(res => res.json())
+        .then(data =>{
+            let temp = data.main.temp;
+            let celsius = Math.floor(temp - 273.15)
+            let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
+            //cityDom.innerHTML = `${data.name}`
+            /* console.log(data); */
+            
+            var iconCode = data.weather[0].icon;            
+            document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
+        })
+        let morning = document.querySelector('.morning')
+        let night = document.querySelector('.night')
+        let container = document.querySelector('.container')
+        let toggleMode = document.querySelector('#toggle_mode')
+        
+        const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+    ];
+    const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
+    
+    setInterval(()=>{
+    var d = new Date()
+    var numData = d.getDate()
+    var month = monthNames[d.getMonth()]
+    var day = dayNames[d.getDay()]
+    var year = d.getFullYear()
+    var hours = d.getHours()
+    var minutes = d.getMinutes()
+    var seconds = d.getSeconds()
+    
+    const hour = 30*hours + minutes/2
+    const minute = 6 * minutes
+    const second = 6 * seconds 
+    /* var gmtHours = -d.getTimezoneOffset()/60; */  
+    
+    let calendar = document.querySelector('#calendar')
+    calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
+    
+    /* interval  */
+        navigator.getBattery().then(function (battery) {
+            let level = battery.level;   
+            /* console.log;    */  
+            document.querySelector('#battery').innerHTML = (Math.floor(level *  100)) + '%';
+            let batteryIcon = document.querySelector('.battery-icon');
+            if(level <=0.10){
+                batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
+            } else if(level <= 0.25){
+                batteryIcon.classList.add('fas', 'fa-battery-quarter')
+            } else if(level <=0.50){
+                batteryIcon.classList.add('fas', 'fa-battery-half')
+            }else if(level <=0.80){
+                batteryIcon.classList.add('fas', 'fa-battery-three-quarters')
+            } else{
+                batteryIcon.classList.add('fas', 'fa-battery-full')
+            }        
+        });
+
+        if(hours >= 12){
+            var period = 'pm'
+        }else{
+            var period = 'am'
+        }
+        document.querySelector('.med').innerHTML = period;
+        document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
+        document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
+        document.querySelector('.sec').style.transform = `rotate(${second}deg)`;
+        
+        
+    },900);
+
+    function toggle() {
+        
+        morning.classList.toggle('toggle_active')
+        night.classList.toggle('toggle_active')
+        light = false
+        container.classList.toggle('bg-dark')
+    }
+    
+    morning.addEventListener('click', ()=>{
+        toggle()
+        toggleMode.innerHTML = 'DARK ON'    
+    })
+    
+    night.addEventListener('click', ()=>{
+        toggle()
+        toggleMode.innerHTML = 'DARK OFF' 
+                 
+    })
+    
+}
+const error = () =>{
+    status.textContent = `Unable to retrieve your location`
 }
 
-morning.addEventListener('click', ()=>{
-    toggle()        
-})
+navigator.geolocation.getCurrentPosition(success,error);
+}
 
-night.addEventListener('click', ()=>{
-    toggle()        
-})
-
-navigator.getBattery().then(function batt (battery) {
-    let level = battery.level;        
-    let batteryIcon = document.querySelector('.battery-icon');
-    
-    document.querySelector('#battery').innerHTML = (Math.ceil(level *  100)) + '%';
-    
-    if(level <=0.10){
-        batteryIcon.classList.add('fas', 'fa-battery-empty','tc-red')
-    } else if(level <= 0.25){
-        batteryIcon.classList.add('fas', 'fa-battery-quarter')
-    } else if(level <=0.50){
-        batteryIcon.classList.add('fas', 'fa-battery-half')
-    }else if(level <=0.80){
-        batteryIcon.classList.add('fas', 'fa-battery-three-quarters')
-    } else{
-        batteryIcon.classList.add('fas', 'fa-battery-full')
-    }        
-    
-    
-    const findMyState = () =>{        
-        let cityDom = document.querySelector('#city')
-        const status =  document.querySelector('.status');
-        const success = (position) => {
-            console.log(position);
-            const latitude = /* 37.9908372 */ position.coords.latitude;
-            const longitude = /* 23.7383394 */ position.coords.longitude;
-            
-            const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=it`
-            
-            fetch(geoApiUrl)
-            .then(res => res.json())
-            .then(data => {
-                cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
-                console.log(data);
-                console.log(`${data.localityInfo.administrative[3].name}`);
-            })             
-            
-            
-            const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
-            
-            fetch(meteoApi)
-            .then(res => res.json())
-            .then(data =>{
-                let temp = data.main.temp;
-                let celsius = Math.floor(temp - 273.15)
-                let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
-                //cityDom.innerHTML = `${data.name}`
-                console.log(data);
-                
-                var iconCode = data.weather[0].icon;            
-                document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
-                
-            })
-
-            const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
-            const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
-            
-            setInterval(()=>{
-                var d = new Date()
-                let numData = d.getDate()
-                let month = monthNames[d.getMonth()]
-                let day = dayNames[d.getDay()]
-                let year = d.getFullYear()
-                const hours = d.getHours()
-                const minutes = d.getMinutes()
-                const seconds = d.getSeconds()
-                
-                const hour = 30*hours + minutes/2
-                const minute = 6 * minutes
-                const second = 6 * seconds 
-                /* var gmtHours = -d.getTimezoneOffset()/60; */
-                
-                let calendar = document.querySelector('#calendar')
-                calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
-                
-                /* interval  */
-                
-                
-                if(hours >= 12){
-                    var period = 'pm'
-                }else{
-                    var period = 'am'
-                }
-                document.querySelector('.med').innerHTML = period;
-                document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
-                document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
-                document.querySelector('.sec').style.transform = `rotate(${second}deg)`;
-                
-                batt(battery)
-                console.log((battery.level));
-            },900);   
-        }
-
-        const error = () =>{
-            alert(`Unable to retrieve your location`)
-        }
-        navigator.geolocation.getCurrentPosition(success,error);
-    }
-    document.addEventListener('DOMContentLoaded', findMyState);
-    
-});
+document.addEventListener('DOMContentLoaded', findMyState);
