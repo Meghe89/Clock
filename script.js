d275fbb210a517a0046e3dfa4bf5a9e17ebb519b
Michele Cozzola <cozzolamichele89@gmail.com>
2022-03-15 19:17:04 +0100

    added

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let container = document.querySelector('.container')
 let toggleMode = document.querySelector('#toggle_mode')
 
 function toggle() {
-        
+    
     morning.classList.toggle('toggle_active')
     night.classList.toggle('toggle_active')
     light = false
@@ -20,7 +20,7 @@ morning.addEventListener('click', ()=>{
 night.addEventListener('click', ()=>{
     toggle()
     toggleMode.innerHTML = 'DARK OFF' 
-             
+    
 })
 
 navigator.getBattery().then(function (battery) {
@@ -42,7 +42,7 @@ navigator.getBattery().then(function (battery) {
 });
 
 const findMyState = () =>{
-
+    
     let cityDom = document.querySelector('#city')
     const status =  document.querySelector('.status');
     const success = (position) => {
@@ -57,70 +57,136 @@ const findMyState = () =>{
         .then(data => {
             cityDom.innerHTML = `${data.localityInfo.administrative[3].name}`;
             /* console.log(data); */
-           /*  console.log(`${data.localityInfo.administrative[3].name}`); */
+            /*  console.log(`${data.localityInfo.administrative[3].name}`); */
         })             
         
         const meteoApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f99157b48e819831ffd36608e93d6d3b&lang=it`
         
         fetch(meteoApi)
         .then(res => res.json())
-        .then(data =>{
-            let temp = data.main.temp;
+        .then(meteo =>{
+            /* actual temp */
+            let temp = meteo.main.temp;
             let celsius = Math.floor(temp - 273.15)
             let degree = document.querySelector('#degree').innerHTML = `${celsius}°`
-            //cityDom.innerHTML = `${data.name}`
-            /* console.log(data); */
-            
-            var iconCode = data.weather[0].icon;            
+
+            var iconCode = meteo.weather[0].icon;            
             document.querySelector("#wicon").src =`http://openweathermap.org/img/w/${iconCode}.png`;
+            /* max temp */
+            let maxTemp = meteo.main.temp_max;
+            let maxC = Math.floor(maxTemp - 273.15)            
+            let max = document.querySelector('#max').innerHTML = ` ${maxC}°`;
+
+            /* min temp */
+            let minTemp = meteo.main.temp_min;
+            let minC = Math.floor(minTemp - 273.15)    
+            let min = document.querySelector('#min').innerHTML = ` ${minC}°`;           
+            
+            /* sun  */
+            function Time (timestamp){
+                var date = new Date(timestamp * 1000);
+              var hours = date.getHours();
+              var minutes = "0" + date.getMinutes();
+              var formattedTime = `${hours}:${minutes.substr(-2)}`;  
+                return formattedTime
+              }
+            let sunrise = document.querySelector('#sunrise').innerHTML = ` ${Time(meteo.sys.sunrise)}`;
+            let sunset = document.querySelector('#sunset').innerHTML = ` ${Time(meteo.sys.sunset)}`; 
+            
+            /* press */
+            console.log(meteo);
+            let press = document.querySelector('#press').innerHTML = ` ${meteo.main.pressure} hPa`; 
+            let windSpeed = (meteo.wind.speed * 3.6).toFixed(2)
+            let wind = document.querySelector('#wind').innerHTML = ` ${windSpeed} m/s`; 
+            let hum = document.querySelector('#hum').innerHTML = ` ${meteo.main. humidity} %`; 
+            
+           
+                  
+            
+           
+        
+            
         })
-       
+        
         
         const monthNames = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
-    const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
-    
-    setInterval(()=>{
-    var d = new Date()
-    var numData = d.getDate()
-    var month = monthNames[d.getMonth()]
-    var day = dayNames[d.getDay()]
-    var year = d.getFullYear()
-    var hours = d.getHours()
-    var minutes = d.getMinutes()
-    var seconds = d.getSeconds()
-    
-    const hour = 30*hours + minutes/2
-    const minute = 6 * minutes
-    const second = 6 * seconds 
-    /* var gmtHours = -d.getTimezoneOffset()/60; */  
-    
-    let calendar = document.querySelector('#calendar')
-    calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
-    
-    /* interval  */
+        const dayNames = ["Domenica","Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato"]
         
-
-        if(hours >= 12){
-            var period = 'pm'
-        }else{
-            var period = 'am'
-        }
-        document.querySelector('.med').innerHTML = period;
-        document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
-        document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
-        document.querySelector('.sec').style.transform = `rotate(${second}deg)`;
+        setInterval(()=>{
+            var d = new Date()
+            var numData = d.getDate()
+            var month = monthNames[d.getMonth()]
+            var day = dayNames[d.getDay()]
+            var year = d.getFullYear()
+            var hours = d.getHours()
+            var minutes = d.getMinutes()
+            var seconds = d.getSeconds()
+            
+            const hour = 30*hours + minutes/2
+            const minute = 6 * minutes
+            const second = 6 * seconds 
+            /* var gmtHours = -d.getTimezoneOffset()/60; */  
+            
+            let calendar = document.querySelector('#calendar')
+            calendar.innerHTML = `${day}, ${numData} ${month} ${year}`
+            
+            /* interval  */
+            
+            
+            if(hours >= 12){
+                var period = 'pm'
+            }else{
+                var period = 'am'
+            }
+            document.querySelector('.med').innerHTML = period;
+            document.querySelector('.hr').style.transform = `rotate(${hour}deg)`;
+            document.querySelector('.min').style.transform = `rotate(${minute}deg)`;
+            document.querySelector('.sec').style.transform = `rotate(${second}deg)`;          
+            
+            
+            
+        },900);
         
         
-    },900);
-
+        
+    }
+    const error = () =>{
+        alert(`Error`)
+    }
     
-    
-}
-const error = () =>{
-    alert(`Error`)
-}
-
-navigator.geolocation.getCurrentPosition(success,error);
+    navigator.geolocation.getCurrentPosition(success,error);
 }
 
 document.addEventListener('DOMContentLoaded', findMyState);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
